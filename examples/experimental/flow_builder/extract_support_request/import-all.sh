732a0862be3b1b6f427740748bcb1f1bcf2bf0da
Allen Chan <avchan@ca.ibm.com>
2025-05-30 14:26:22 -0400

    fix(flows): Update import-all.sh for flows example (#1137)

--- examples/experimental/flow_builder/extract_support_request/import-all.sh
+++ examples/experimental/flow_builder/extract_support_request/import-all.sh
@@ -4,8 +4,8 @@
 orchestrate env activate local
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-#ADK=$(dirname "$(dirname "$(dirname "$(dirname "$SCRIPT_DIR")")")")
-#export PYTHONPATH=$PYTHONPATH:$ADK:$ADK/src 
+ADK=$(dirname "$(dirname "$(dirname "$(dirname "$SCRIPT_DIR")")")")
+export PYTHONPATH=$PYTHONPATH:$ADK:$ADK/src 
 
 for python_tool in  email_helpdesk.py; do
   orchestrate tools import -k python -f ${SCRIPT_DIR}/tools/${python_tool} -r ${SCRIPT_DIR}/tools/requirements.txt
