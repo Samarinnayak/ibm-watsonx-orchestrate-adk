e70d55b76a66e51e21996d13aa3efb0dc2d82c72
ERIC MARCOUX <Eric.Marcoux@ibm.com>
2025-06-17 08:32:13 -0400

    fix(cli): Shows hidden agents and assistants (#1243)
    Adds include_hidden to agent and assistant listing calls
    
    Closes WatsonOrchestrate/wxo-internal-support#78

--- src/ibm_watsonx_orchestrate/client/agents/agent_client.py
+++ src/ibm_watsonx_orchestrate/client/agents/agent_client.py
@@ -16,7 +16,7 @@ class AgentClient(BaseAPIClient):
         return self._post(self.base_endpoint, data=payload)
 
     def get(self) -> dict:
-        return self._get(self.base_endpoint)
+        return self._get(f"{self.base_endpoint}?include_hidden=true")
 
     def update(self, agent_id: str, data: dict) -> dict:
         return self._patch(f"{self.base_endpoint}/{agent_id}", data=data)
@@ -29,7 +29,7 @@ class AgentClient(BaseAPIClient):
 
     def get_drafts_by_names(self, agent_names: List[str]) -> List[dict]:
         formatted_agent_names = [f"names={x}" for x  in agent_names]
-        return self._get(f"{self.base_endpoint}?{'&'.join(formatted_agent_names)}")
+        return self._get(f"{self.base_endpoint}?{'&'.join(formatted_agent_names)}&include_hidden=true")
     
     def get_draft_by_id(self, agent_id: str) -> List[dict]:
         if agent_id is None:
@@ -45,5 +45,5 @@ class AgentClient(BaseAPIClient):
     
     def get_drafts_by_ids(self, agent_ids: List[str]) -> List[dict]:
         formatted_agent_ids = [f"ids={x}" for x  in agent_ids]
-        return self._get(f"{self.base_endpoint}?{'&'.join(formatted_agent_ids)}")
+        return self._get(f"{self.base_endpoint}?{'&'.join(formatted_agent_ids)}&include_hidden=true")
     
