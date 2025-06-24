2387e09092e8d84e8c1cd53f2160026be811b370
Benen Bruen <benenbruen@ibm.com>
2025-06-24 14:44:25 +0100

    remove test agent

--- examples/agent_builder/sample_agent/remove_all.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -x
-
-#orchestrate env activate local
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-orchestrate agents remove -n sample_agent -k native
