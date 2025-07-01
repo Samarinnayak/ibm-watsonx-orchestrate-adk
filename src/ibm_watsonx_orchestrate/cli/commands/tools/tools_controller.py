89bd17c71c6669fd46effacdf70870c530357280
Darragh McGonigle <Darragh.McGonigle@ibm.com>
2025-07-01 17:57:48 +0100

    fix(connections): Change error to warning for live (#1374)

--- src/ibm_watsonx_orchestrate/cli/commands/tools/tools_controller.py
+++ src/ibm_watsonx_orchestrate/cli/commands/tools/tools_controller.py
@@ -114,10 +114,12 @@ def validate_app_ids(kind: ToolKind, **args) -> None:
                 conn = imported_connection.get(conn_environment)
 
                 if conn is None or conn.security_scheme is None:
-                    logger.error(f"Connection '{app_id}' is not configured in the '{conn_environment}' environment.")
+                    message = f"Connection '{app_id}' is not configured in the '{conn_environment}' environment."
                     if conn_environment == ConnectionEnvironment.DRAFT:
+                        logger.error(message)
                         sys.exit(1)
-                    logger.error("If you deploy this tool without setting the live configuration the tool will error during execution.")
+                    else:
+                        logger.warning(message + " If you deploy this tool without setting the live configuration the tool will error during execution.")
                     continue
 
                 if conn.security_scheme == ConnectionSecurityScheme.KEY_VALUE:
@@ -235,10 +237,12 @@ def validate_python_connections(tool: BaseTool):
             conn = imported_connection.get(conn_environment)
             conn_identifier = conn.app_id if conn is not None else connection_id
             if conn is None or conn.security_scheme is None:
-                logger.error(f"Connection '{conn_identifier}' is not configured in the '{conn_environment}' environment.")
+                message = f"Connection '{conn_identifier}' is not configured in the '{conn_environment}' environment."
                 if conn_environment == ConnectionEnvironment.DRAFT:
+                    logger.error(message)
                     sys.exit(1)
-                logger.error("If you deploy this tool without setting the live configuration the tool will error during execution.")
+                else:
+                    logger.warning(message + " If you deploy this tool without setting the live configuration the tool will error during execution.")
                 continue
 
             imported_connection_auth_type = get_connection_type(security_scheme=conn.security_scheme, auth_type=conn.auth_type)
